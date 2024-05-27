d8e5a60f990161ef315ee6ef775474fc8ffe7fb0
sophianortmann <sophia.nortmann@gmx.de>
2024-05-27 15:23:53 +0000

    js changes color of answer when answer is submitted

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 const submitButton = document.querySelector("button.submit");
-console.log("Hello",submitButton);
+
+function checkAnswer(selectedAnswer) {
+    if (selectedAnswer.dataset.answer === selectedAnswer.value) {
+        selectedAnswer.classList.add("right-answer");
+    } else {
+        selectedAnswer.classList.add("wrong-answer");
+    }
+
+}
+
+function solution(event) {
+    const selectedAnswers = document.querySelectorAll("select.challenge")
+    selectedAnswers.forEach(checkAnswer);
+}
+
+submitButton.addEventListener("click", solution);
