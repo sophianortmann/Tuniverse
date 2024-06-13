25b00b5c9bbe90c525b829c6fe3f9b7608b303fb
sophianortmann <sophia.nortmann@gmx.de>
2024-06-13 10:16:38 +0000

    feat: add images

--- selectChallenge.js
+++ selectChallenge.js
@@ -6,5 +6,5 @@ buttonToFirstChallenge.addEventListener("click", goToFirstChallenge
 );
 
 function goToFirstChallenge() {
-    open('https://laughing-space-goggles-x59w9x7r7qjg36j4p-3000.app.github.dev/firstChallenge.html', '_self');
+    open('/firstChallenge.html', '_self');
 }
